7ad93d5d6826d2beb12a67e49e6b4025ad7b50f7
Edier Fabian Hernandez <edierfabian@gmail.com>
2021-03-01 22:41:46 -0500

    Modificacion del @GetMapping("/persons") y creacion del@GetMapping("/persons/{id}")

--- src/main/java/com/qbo/controller/PersonController.java
+++ src/main/java/com/qbo/controller/PersonController.java
@@ -1,12 +1,19 @@
 package com.qbo.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import javax.websocket.server.PathParam;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.qbo.exception.ResourceNotFoundException;
 import com.qbo.model.Person;
 import com.qbo.service.PersonService;
 
@@ -19,10 +26,27 @@ public class PersonController {
 	
 	
 	@GetMapping("/persons")
-	public List<Person> getAllPersons(){
+	public ResponseEntity<List<Person>> getAllPersons(){
 		
+		List<Person> persons=new ArrayList<>();
+		personService.getAllPersons().forEach(persons::add);
+		if(persons.isEmpty()) {
+			return new ResponseEntity<>(HttpStatus.NO_CONTENT);			
+		}
+		return new ResponseEntity<>(personService.getAllPersons(), HttpStatus.OK);
 		
-		return personService.getAllPersons();
+	}
+	
+	@GetMapping("/persons/{id}")
+	public ResponseEntity<Person> getPersonById(@PathVariable ("id") Integer id) 
+			throws ResourceNotFoundException{
+		
+		Person person= personService
+				.findById(id)
+				.orElseThrow(()-> 
+				new ResourceNotFoundException("Not Found Person Whit id = "+id));
+		return new ResponseEntity<>(person,HttpStatus.OK);
+		
 		
 	}
 
